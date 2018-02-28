b9128f7e9240987a2bfb87697f844b276a9fecc5
ShanKaiying <34665653+ShanKaiying@users.noreply.github.com>
2018-02-28 09:09:18 +0800

    Update Bicycle.java

--- StudyNotes/Bicycle.java
+++ StudyNotes/Bicycle.java
@@ -2,22 +2,29 @@ public class Bicycle{
 public static int NumberOfWheels = 2;
 public String brand;
 public double weight;
+public double speed;
 boolean isRunning;
 
 public Bicycle(){
  this.brand = "";
  this.weight = 10.0;
  isRunning = false;
+ this.speed = 10.;
 }
 
-public Bicycle(String brand, double weight, double speed, double position, boolean isRunning){
+public Bicycle(String brand, double weight, double speed, double position, boolean isRunning, double speed){
  this.brand = brand;
  this.weight = weight;
  this.isRunning = isRunning;
+ this.speed = speed;
 }
 
 public boolean ride(){
  return isRunning;
+}
+ 
+public void accelerate(){
+ speed +=3;
 }
 
 }
